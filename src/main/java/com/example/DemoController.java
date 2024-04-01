151079c6374d9640dd7044791e7274ff21a23af2
takethegame <zengyunhao@zengyunhaideMacBook-Pro.local>
2024-04-01 17:39:29 +0800

    init

---
+++ src/main/java/com/example/DemoController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/demo")
+    @ResponseBody
+    public String demo() {
+        return "{\"test\":\"test\"}";
+    }
+}
