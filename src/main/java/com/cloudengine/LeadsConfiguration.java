bd2d91b3ebeff2e8f5de8dabd21e69e69e2c2a15
takethegame <zengyunhao@zengyunhaideMacBook-Pro.local>
2024-04-03 20:02:22 +0800

    leads

--- src/main/java/com/cloudengine/LeadsConfiguration.java
+++ src/main/java/com/cloudengine/LeadsConfiguration.java
@@ -1,9 +1,15 @@
 package com.cloudengine;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,9 +18,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -40,4 +46,47 @@ public class LeadsConfiguration {
                 .build();
     }
 
+    @Value("{spring.datasource.url}")
+    private String url;
+
+    @Value("{spring.datasource.username}")
+    private String username;
+
+    @Value("{spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        MysqlDataSource mysqlDataSource = new MysqlDataSource();
+        mysqlDataSource.setPassword(password);
+        mysqlDataSource.setUser(username);
+        mysqlDataSource.setURL(url);
+        return mysqlDataSource;
+    }
+
+    @Value("${mybatis.typeAliasPackage}")
+    private String typeAliasPackage;
+
+    @Value("${mybatis.mapperLocation}")
+    private String mapperLocation;
+
+    @Value("${mybatis.configLocation}")
+    private String configLocation;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setTypeAliasesPackage(typeAliasPackage);
+        List<Resource> rList = Arrays.asList(new PathMatchingResourcePatternResolver().getResource(mapperLocation));
+        sessionFactoryBean.setMapperLocations(rList.toArray(new Resource[rList.size()]));
+//        sessionFactoryBean.setConfigLocation(new DefaultResourceLoader().getResource(configLocation));
+
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
---
+++ src/main/java/com/cloudengine/dao/mapper/LeadsMapper.java
@@ -0,0 +1,8 @@
+package com.cloudengine.dao.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class LeadsMapper {
+}
